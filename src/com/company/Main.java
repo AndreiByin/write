ecb6fdab2f0bd1dd102d3e8bcb20d57d98c8ec65
AndreiBy <andreialshuk@gmail.com>
2020-03-18 13:48:37 +0300

    (Отказано в доступе)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,20 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("c:\\temp\\"));
+            String tx;
+            while (!(tx=br.readLine()).equals("stop")){
+                System.out.println(tx);
+                bw.write(tx + "/n");
+                bw.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
